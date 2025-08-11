37fcd00a7b4e887ea76ded22ba9bff97e17ab403
narsingh24 <narsingh@iclimbs.com>
2025-08-11 10:44:03 +0530

    update port same for backend as using in front-end

--- packages/back-end/src/server.ts
+++ packages/back-end/src/server.ts
@@ -5,15 +5,21 @@ import app from "./app";
 import { logger } from "./util/logger";
 import { getAgendaInstance } from "./services/queueing";
 
-const server = app.listen(app.get("port"), () => {
+const PORT = process.env.PORT || 10000;
+const server = app.listen(PORT, () => {
   logger.info(
     `Back-end is running at http://localhost:${app.get("port")} in ${app.get(
       "env"
     )} mode. Press CTRL-C to stop`
   );
 });
-
-export default server;
+// const server = app.listen(app.get("port"), () => {
+//   logger.info(
+//     `Back-end is running at http://localhost:${app.get("port")} in ${app.get(
+//       "env"
+//     )} mode. Press CTRL-C to stop`
+//   );
+// });
 
 process.on("unhandledRejection", (rejection: unknown) => {
   if (["string", "number", "boolean"].includes(typeof rejection)) {
